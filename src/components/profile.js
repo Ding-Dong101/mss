862432506ee7f1879d75a294b178d75db106b412
Ding-Dong101 <kwawerastus2@gmail.com>
2025-09-05 19:17:10 +0000

    designs

---
+++ src/components/profile.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Bell, Plus, Send, ThreeDots } from "react-icons/fa";
+import "../css/profile.css";
+
+let userImage = "http://picsum.photos/50/50";
+let userName = "John Doe";
+let accountBalance = 5000.0;
+let WithdrawableAmount = 4000.0;
+let nationality = "🇬🇭";
+export default function Profile() {
+  return (
+    <>
+      <div className="profile-container">
+        <div className="profile">
+          <div className="profile-object">
+            <img src={userImage} alt="user profile" />
+            <div className="greeting">
+              <p>
+                Hello,<span className="bold">{" " + userName}</span>
+                <span> {nationality}</span>
+              </p>
+            </div>
+          </div>
+          <button className="notification-btn btn">
+            <Bell size={24} />
+          </button>
+        </div>
+        <div className="account-details">
+          <div className="account-balance account">
+            <p>Account Balance</p>
+            <p className="bold money">{"GHC " + accountBalance}</p>
+          </div>
+          <div className="withdraw-balance">
+            <p>Withdrawable Amount</p>
+            <p className="bold money">{"GHC " + WithdrawableAmount}</p>
+          </div>
+        </div>
+      </div>
+      <div className="quick-actions">
+        <button className="action-btn btn">
+          <Plus size={24} />
+        </button>
+        <button className="action-btn btn">
+          <Send size={24} />
+        </button>
+
+        <button className="action-btn btn">
+          <ThreeDots size={24} />
+        </button>
+      </div>
+    </>
+  );
+}
